91eebbf5f64540c0bd2ee5d482c712164470fdb7
Samuel Oliveira <112700596+sammueloliveira@users.noreply.github.com>
2023-05-20 15:05:51 -0300

    Add project files.

---
+++ Infra/Data/Contexto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Presentation.Models;
+
+namespace Infra.Data
+{
+    public  class Contexto : IdentityDbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) :base(options)
+        {
+
+        }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(ObterConexao());
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        private string ObterConexao()
+        {
+            return "Data Source=samuel;Initial Catalog=Minima_Api;Integrated Security=True;Encrypt=false";
+        }
+    }
+}
---
+++ Minimal_Api/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Minimal_Api
+{
+    public class Class1
+    {
+
+    }
+}
